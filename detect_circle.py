8fe552731287ec59258fe1c077a01021f6ffd07c
Cedric Van Goethem <vangoethem.c@gmail.com>
2015-10-14 12:38:55 +0200

    Blahh

--- detect_circle.py
+++ detect_circle.py
@@ -11,7 +11,9 @@ from skimage.util import img_as_ubyte
 
 
 # Load picture and detect edges
-im = io.imread('data/train/blue_circles/D1b/00638_01854.png')
+#im = io.imread('data/train/blue_circles/D1b/02583_06123.png')
+im = io.imread('data/train/rectangles_up/B21/00969_01994.png')
+
 img_gray = rgb2gray(im)
 edges = canny(img_gray, sigma=2)
 
@@ -21,24 +23,18 @@ fig, ax = plt.subplots(ncols=1, nrows=1, figsize=(5, 2))
 # calculate image diameter
 shape = im.shape
 diam = math.sqrt(shape[0]**2 + shape[1]**2)
-hough_res = hough_circle(edges, np.arange(diam/3, diam * 0.8, 2))
+radii = np.arange(diam/3, diam * 0.8, 2)
+hough_res = hough_circle(edges, radii)
 
 
-centers = []
-accums = []
-radii = []
+peaks = peak_local_max(hough_res, num_peaks=1, min_distance=1, threshold_abs=0.01, exclude_border=True)
+if len(peaks) > 0:
+    radius = radii[peaks[0][0]]
+    x_circle = peaks[0][1]
+    y_circle = peaks[0][2]
 
-for radius, h in zip(hough_radii, hough_res):
-    peaks = peak_local_max(h, num_peaks=1)
-    centers.extend(peaks)
-    accums.extend(h[peaks[:, 0], peaks[:, 1]])
-    radii.extend([radius] * num_peaks)
-
-# Draw the most prominent 5 circles
-for idx in np.argsort(accums)[::-1][:5]:
-    center_x, center_y = centers[idx]
-    radius = radii[idx]
-    cx, cy = circle_perimeter(center_y, center_x, radius)
-    #image[cy, cx] = (220, 20, 20)
+    circle = plt.Circle((x_circle, y_circle), radius=radius, fc='y', alpha=0.4)
+    plt.gca().add_patch(circle)
 
 ax.imshow(im, cmap=plt.cm.jet)
+plt.show()
