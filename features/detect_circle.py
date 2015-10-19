75240da55d4310252ad0c04fe8a33faf0388173b
unknown <benjamin.twechuizen@ugent.be>
2015-10-19 19:46:35 +0200

    Probably approximately fix for low-contrast images error

--- features/detect_circle.py
+++ features/detect_circle.py
@@ -8,18 +8,21 @@ from skimage.feature import peak_local_max, canny
 from skimage.draw import circle_perimeter
 from skimage.color import rgb2gray
 from skimage.util import img_as_ubyte
+from skimage import exposure
 
 
 from features import AbstractFeature
+from visualize import *
 
 class DetectCircle(AbstractFeature):
 
     def __init__(self):
         pass
 
-    def process(self,im):
+    def process(self, im):
         img_gray = rgb2gray(im)
-        edges = canny(img_gray, sigma=0.5)
+        img_adapted = exposure.equalize_hist(img_gray)
+        edges = canny(img_adapted, sigma=3)
 
         # Detect two radii
         # calculate image diameter
@@ -28,12 +31,12 @@ class DetectCircle(AbstractFeature):
         radii = np.arange(diam/3, diam * 0.8, 2)
         hough_res = hough_circle(edges, radii)
 
-        # peaks = peak_local_max(hough_res, num_peaks=1, min_distance=1, threshold_abs=0.01, exclude_border=True)
         accums = []
         for radius, h in zip(radii, hough_res):
             # For each radius, extract two circles
             peaks = peak_local_max(h, num_peaks=1, min_distance=1)
             accums.extend(h[peaks[:, 0], peaks[:, 1]])
+
 
         idx = np.argsort(accums)[::-1][1]
         return accums[idx]
