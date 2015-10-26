8b3db2f0337994f15411fcd17a3cf39050747748
Cedric Van Goethem <vangoethem.c@gmail.com>
2015-10-26 22:32:28 +0100

    Added smarter folding algorithm

--- image_loader.py
+++ image_loader.py
@@ -12,8 +12,12 @@ class LabelledImage:
         self.super_label = superLabel
         self.features = None
 
+    def __str__(self):
+        return self.filename
+
 
 def load(directories, is_train_data, permute=True):
+    print('Loading images. Train: %r' % is_train_data)
     values = []
     for directory in directories:
         for dirpath, dirnames, _ in os.walk(directory):
