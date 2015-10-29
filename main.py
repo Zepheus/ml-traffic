28d3808d8c8ea35aab301dd77e21ec6658f23489
Cedric Van Goethem <cedric@Cedrics-MacBook-Pro.local>
2015-10-29 17:28:36 +0100

    Added feature in folder

--- main.py
+++ main.py
@@ -6,10 +6,13 @@ from features import *
 from Load import *
 from image_loader import *
 from cross_validation import cross_validate
+from skimage.color import *
+from visualize import *
 from MetaParameterEstimators import *
 
 
-def train_and_predict(trainer_function, feature_combiner, number_of_pca_components=0, train_directories=['data/train'], test_directories=['data/test']):
+def train_and_predict(trainer_function, feature_combiner, number_of_pca_components=0, train_directories=['data/train'],
+                      test_directories=['data/test']):
     # Load data
     trainer = trainer_function()
     train_images = load(train_directories, True)
@@ -42,36 +45,40 @@ def train_and_predict(trainer_function, feature_combiner, number_of_pca_componen
         file.write('%d,%s\n' % (identifier, str.join(',', [('%.13f' % p) for p in predictions[0]])))
     file.close()
 
+
 def trainFolds(directories):
     images = load(directories, True, permute=True)
-    combiner = FeatureCombiner([HsvFeature(), DetectCircle(), HogFeature()])  # Feature selection
+    combiner = FeatureCombiner([HsvFeature(), DetectCircle(), HogFeature(), RegionRatio(), DetectSymmetry()])  # Feature selection
     trainer = LogisticRegressionTrainer  # Learning algorithm, make sure this is a function and not an object
-    cross_validate(images, combiner, trainer, k=10, use_super_class=False, number_of_pca_components=0)  # use 10 folds, no pca
+    cross_validate(images, combiner, trainer, k=10, use_super_class=False,
+                   number_of_pca_components=0)  # use 10 folds, no pca
+
 
 def featureTest(directories):
     images = load(directories, True, permute=False)
     combiner = ColorCenter()  # Feature selection
-    trainer = GaussianNaiveBayes # Learning algorithm, make sure this is a function and not an object
+    trainer = GaussianNaiveBayes  # Learning algorithm, make sure this is a function and not an object
     values = [combiner.process(im.image) for im in images if im.label == 'D10']
     values2 = [combiner.process(im.image) for im in images if im.label == 'B21']
-    labels = ['D10','B21']
+    labels = ['D10', 'B21']
     from visualize import ScatterPlot
-    plot =  ScatterPlot()
-    plot.show(labels,[values,values2])
+    plot = ScatterPlot()
+    plot.show(labels, [values, values2])
+
 
 def estimateMetas(directories):
-    meta_estimators = [estimateHogOrientationsParameters,estimateHogPixelsPerCellParameters,
+    meta_estimators = [estimateHogOrientationsParameters, estimateHogPixelsPerCellParameters,
                        estimateHogCellsPerBlockParameters,
                        estimateDetectCircleParameters
-                   ]
+                       ]
 
     for estimator in meta_estimators:
         estimator(directories)
 
-#train_and_predict(trainer_function, FeatureCombiner([HsvFeature(), DetectCircle(), HogFeature()]), 0,
+# train_and_predict(trainer_function, FeatureCombiner([HsvFeature(), DetectCircle(), HogFeature()]), 0,
 #                  ['data/train'],
 #                  ['data/test'])
 
-#trainFolds(['data/train'])
-#estimateMetas(['data/train'])
-#trainFolds(['data/train/blue_circles','reversed_triangles'])
+# trainFolds(['data/train'])
+# estimateMetas(['data/train'])
+# trainFolds(['data/train/blue_circles','reversed_triangles'])
