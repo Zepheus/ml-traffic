5dc3c090dc646719dbdf7d070488dc4414ddf340
Cedric Van Goethem <vangoethem.c@gmail.com>
2015-11-14 15:24:58 +0100

    Fix for 1 dimensional prediction warnings

--- learn/normal_svc.py
+++ learn/normal_svc.py
@@ -1,18 +1,39 @@
 from learn import AbstractLearner
 from sklearn.svm import SVC
+from sklearn import preprocessing
 
 
 class NormalSVCTrainer(AbstractLearner):
 
-    def __init__(self, kernel='rbf', penalty=1.0):
-        self.learner = SVC(C=penalty, kernel=kernel, probability=True)
+    def __init__(self, kernel='linear', penalty=1.0, cache=200, scale=True):
+        self.learner = SVC(C=penalty, kernel=kernel, probability=True, cache_size=cache)
+        self.scale = scale
 
     def _train(self, x_train, y_train):
-        self.learner = self.learner.fit(x_train, y_train)
+        if self.scale:
+            self.scaler = preprocessing.StandardScaler().fit(x_train)
+            x_scaled = self.scaler.transform(x_train)
+            self.learner = self.learner.fit(x_scaled, y_train)
+        else:
+            self.learner = self.learner.fit(x_train, y_train)
 
     def _predict(self, x):
-        return self.learner.predict(x)
+        if self.scale:
+            dimensions = len(x.shape)
+            if dimensions == 1: # fix for 1 dimensional array
+                x = x.reshape(1, -1)
+            x_scaled = self.scaler.transform(x)
+            return self.learner.predict(x_scaled)
+        else:
+            return self.learner.predict(x)
 
     def predict_proba(self, x):
-        return self.learner.predict_proba(x)
+        if self.scale:
+            dimensions = len(x.shape)
+            if dimensions == 1: # fix for 1 dimensional array
+                x = x.reshape(1, -1)
+            x_scaled = self.scaler.transform(x)
+            return self.learner.predict_proba(x_scaled)
+        else:
+            return self.learner.predict_proba(x)
 
--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from MetaParameterEstimators import *
 from cross_validation import *
 from prediction import *
 
-#train_and_predict(lambda: LogisticRegressionTrainer(181),
-#                  [HsvFeature(), DetectCircle(sigma=1.8), HogFeature(orientations=5, pixels_per_cell=(8, 8),
-#                                                                     cells_per_block=(3, 3), resize=96),
-#                   DetectSymmetry(blocksize=2, size=96), RegionRatio()], 0,
-#                  ['data/train'], ['data/test'])
+train_and_predict(lambda: NormalSVCTrainer(kernel='linear', cache=1000, penalty=0.85, scale=False),
+                  [HsvFeature(), DetectCircle(sigma=1.8), HogFeature(orientations=5, pixels_per_cell=(8, 8),
+                                                                     cells_per_block=(3, 3), resize=96),
+                   DetectSymmetry(blocksize=2, size=96), RegionRatio()], 0,
+                  ['data/train'], ['data/test'])
 
-trainFolds(["data/train"], lambda: NormalSVCTrainer(kernel='linear'))
+# trainFolds(["data/train"], lambda: NormalSVCTrainer(kernel='linear', cache=1000, penalty=0.85, scale=False)) # mean error_ratio is 0.083940 (std: 0.008505)
 #estimateMetas(['data/train'], lambda: LogisticRegressionTrainer(181.0))
