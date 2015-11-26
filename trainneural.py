1a72fc7e731c481ec8916ae381fe9c675c85c749
Cedric Van Goethem <vangoethem.c@gmail.com>
2015-11-26 20:21:19 +0100

    Variation

--- trainneural.py
+++ trainneural.py
@@ -87,18 +87,18 @@ def build_cnn(input_size, input_var=None):
     network = lasagne.layers.InputLayer(shape=(None, 3, input_size, input_size), input_var=input_var)
 
     network = lasagne.layers.Conv2DLayer(
-        network, num_filters=32, filter_size=(6, 6),
+        network, num_filters=32, filter_size=(5, 5),
         nonlinearity=lasagne.nonlinearities.rectify,
         W=lasagne.init.GlorotUniform())
     network = lasagne.layers.MaxPool2DLayer(network, pool_size=(2, 2))
 
     network = lasagne.layers.Conv2DLayer(
-        network, num_filters=64, filter_size=(5, 5),
+        network, num_filters=32, filter_size=(5, 5),
         nonlinearity=lasagne.nonlinearities.rectify)
     network = lasagne.layers.MaxPool2DLayer(network, pool_size=(2, 2))
 
     network = lasagne.layers.Conv2DLayer(
-        network, num_filters=32, filter_size=(3, 3),
+        network, num_filters=32, filter_size=(3, 1),
         nonlinearity=lasagne.nonlinearities.rectify)
     network = lasagne.layers.MaxPool2DLayer(network, pool_size=(2, 2))
 
@@ -110,7 +110,7 @@ def build_cnn(input_size, input_var=None):
 
     network = lasagne.layers.DenseLayer(
         lasagne.layers.dropout(network, p=0.5),
-        num_units=100,
+        num_units=150,
         nonlinearity=lasagne.nonlinearities.rectify)
 
     network = lasagne.layers.DenseLayer(
